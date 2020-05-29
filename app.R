bb2055f6eb670d64be5afce31e9de03acd0493d4
Luis Alberto Flores Rodriguez <histeralberto@gmail.com>
2020-05-29 17:50:30 -0500

    actualizacion del 29 de mayo

--- app.R
+++ app.R
@@ -11,7 +11,7 @@ library(plotly)
 ui <- fluidPage(
   
   # Titulo
-  titlePanel("COVID19 en M\u00E9xico. Registros reportados por la Sr\u00EDa de Salud al 8 de Mayo"),
+  titlePanel("COVID19 en M\u00E9xico. Registros reportados por la Sr\u00EDa de Salud al 28 de Mayo"),
   
   #sidebarPanel(
  
@@ -29,6 +29,7 @@ ui <- fluidPage(
   plotlyOutput("disPlot2"),
   plotlyOutput("barPlot"),
   h5("* El indicador de vulnerabilidad no considera variables importantes como: Indicador de pobreza multidimencional, nutrici\u00F3n, poblaciones ind\u00EDgenas, acceso a la salud."),
+  h5("* Los dados se refieren a la Fecha de Inicio de S\u00EDntomas"), 
   h5("Elaborado por Luis Alberto Flores"), 
   h5("Fuente de los datos:"),
   a("https://www.gob.mx/salud/documentos/datos-abiertos-152127ab", href="https://www.gob.mx/salud/documentos/datos-abiertos-152127ab"),
@@ -42,7 +43,7 @@ ui <- fluidPage(
 server <- function(input, output) {
   
 
-  # carga csv de NOmbres de Mun, con cve, pob, Va.
+  # Carga de csv de enrrequecimiento que contiene: Nombres de Mun, CVE, POB, VA
   html_shp <- readLines('http://www.mediafire.com/file/frxzqg0j9blnq9a/nombres_mun.csv/file')
   rg_shp <- html_shp[grep("Download file", html_shp, perl = F)+1  ]
   rg2_shp <- sub("\\W+href=\\\"", "", rg_shp)
@@ -50,34 +51,34 @@ server <- function(input, output) {
   rm(html_shp)
   nom_mundf <- read.csv(rg3_shp, sep = ",", header = T, stringsAsFactors = F, encoding = "latin1")
   nom_mundf$cve_mun <- as.character(nom_mundf$cve_mun)
-  # "nombres_mun.csv"
-  # arreglar claves.
+
+    # Adecuar claves CVE municipales (concatenar clave de entidad con clave municipal)
   nom_mundf <- nom_mundf %>% 
     mutate(cve_mun = case_when(
       nchar(.$cve_mun) == 4  ~ paste0("0", .$cve_mun),
       nchar(.$cve_mun) == 5  ~  .$cve_mun
       
     )
-    )
+  )
   
+  # Convertir POB a nimerico y obtener per capita
   nom_mundf$Poblacion <- as.numeric(nom_mundf$Poblacion)
   nom_mundf$PerCapita <- nom_mundf$Valor_Agregado / nom_mundf$Poblacion
-  
 
-  # CArgar info covid (se actualiza el nombre de archivo, previamente se suebe a media fire)
-  html_shp <- readLines('https://www.mediafire.com/file/z7rj0fybgsw0i15/200506COVID19MEXICO.csv/file')
+  # Cargar csv de info covid (se actualiza el nombre de archivo. El archivo se aloja día a día en media fire)
+  html_shp <- readLines('https://www.mediafire.com/file/i2ne3w7iltu3otx/200522COVID19MEXICO.csv/file')
   rg_shp <- html_shp[grep("Download file", html_shp, perl = F)+1  ]
   rg2_shp <- sub("\\W+href=\\\"", "", rg_shp)
   rg3_shp <- sub("\\\">", "", rg2_shp)
   cov_mun <- read.csv(rg3_shp, sep = ",", header = T, stringsAsFactors = F)
   # "200502COVID19MEXICO.csv"
-  # crear claves
+  # crear claves para el csv de covid
   cov_mun <- cov_mun %>% 
     mutate(ent_ = case_when(
       nchar(.$ENTIDAD_RES) == 1  ~ paste0("0",.$ENTIDAD_RES),
       nchar(.$ENTIDAD_RES) == 2  ~ paste0(.$ENTIDAD_RES)
     )
-    )
+  )
   
   cov_mun <- cov_mun %>% 
     mutate(mun_ = case_when(
@@ -85,9 +86,9 @@ server <- function(input, output) {
       nchar(.$MUNICIPIO_RES) == 2  ~ paste0("0",.$MUNICIPIO_RES),
       nchar(.$MUNICIPIO_RES) == 3  ~ paste0(.$MUNICIPIO_RES)
     )
-    )
+  )
   
-  # unir claves, filtrar positivos
+  # Concatenar las claves creadas y filtrar casos positivos positivos "RESULTADO == 1"
   cov_mun <- cov_mun %>% 
     mutate(cve_mun = paste0(ent_, mun_))
   cov_mun <-  cov_mun %>% filter(RESULTADO == 1)
@@ -96,7 +97,7 @@ server <- function(input, output) {
   cov_mun <- cov_mun %>% mutate(Conteo = 1)
   cov_mun$SEXO <- as.character(cov_mun$SEXO)
   
-  # join por cve de: datos de covid con datos de poblacion
+  # join tomando la cve entre los datos de covid con datos de enrriquecimiento
   mexico_mun_prueba <- as.data.frame(cov_mun$cve_mun)
   names(mexico_mun_prueba) <- "cve_mun"
   cov_mun <- left_join(x = cov_mun, y = nom_mundf, by= "cve_mun")
@@ -105,6 +106,7 @@ server <- function(input, output) {
   
   #########################
   # agregaciones
+  ########################
   
   # acum nacional y porcentaje (grafico de total nacional)
   casos_by_fecha <- cov_mun %>% group_by(FECHA_SINTOMAS) %>% summarise(count = n()) %>% mutate(Acum = cumsum(count))
@@ -120,7 +122,7 @@ server <- function(input, output) {
   mun_casos_indice <- right_join(x = casos_by_cve_mun, y = nom_mundf, by= "cve_mun") %>%  mutate(medida=PerCapita * Conteo/100000)
   mun_casos_indice <- mun_casos_indice %>% mutate(indice = 1 - (medida / sum(mun_casos_indice$medida, na.rm = T))) %>% arrange(desc(indice))
   
-  
+  View(cov_fecha_num)
   # fecha y municipio confirmados (graficos de lineas por municipio)
   cov_fecha_num <- cov_mun %>% group_by(nom_mun, FECHA_SINTOMAS) %>% summarise(Suma_Acum = sum(Conteo)) %>% 
     arrange(FECHA_SINTOMAS) %>% group_by(nom_mun) %>% mutate(Suma_Acum2 = cumsum(Suma_Acum))
